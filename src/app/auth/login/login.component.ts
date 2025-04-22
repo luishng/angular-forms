a13f4fa8c1fbda7e1bea0359eb84bab769bae91c
Luis Henrique <luishng@gmail.com>
2025-04-22 14:31:51 -0300

    Add first form with template driven approuch

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  imports: [FormsModule],
+})
+export class LoginComponent {
+
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+
+    console.log(formData.form);
+    console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset()
+  }
+}
