de62d8eaaff45e5905f410df1283a83c1788be95
Luis Henrique <luishng@gmail.com>
2025-04-22 14:47:06 -0300

    feat: add localstore to save form value with not finished

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,28 @@ import { FormsModule, NgForm } from '@angular/forms';
   imports: [FormsModule],
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form')
+  private destroyRef = inject(DestroyRef)
+
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form')
+
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm)
+        const savedEmail = loadedFormData.email;
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail)
+        }, 1);
+      }
+
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem("saved-login-form", JSON.stringify({ email: value.email }))
+      })
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe())
+    })
+  }
 
   onSubmit(formData: NgForm) {
     if (formData.form.invalid) {
